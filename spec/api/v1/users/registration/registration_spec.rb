f0dbeecd4d57dd4648ca0cad80aa7514106445a7
Andrew Reid <areid.sj@gmail.com>
2020-08-20 12:27:54 -0600

    Write user registration test

---
+++ spec/api/v1/users/registration/registration_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Registration Endpoint - ', type: :request do
+  before(:each) do
+    @starting_user_count = User.count
+  end
+
+  it 'registers a user' do
+    post 'api/v1/users', params:
+      {
+        'first_name': 'Joe',
+        'last_name': 'Strummer',
+        'email': 'whatever@example.com',
+        'password': 'pw123',
+        'password_confirmation': 'pw123'
+      }
+
+    output = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(output[:data][:id]).to eq(User.last.id.to_s)
+    expect(output[:data][:type]).to eq('users')
+    expect(output[:data][:attributes][:email]).to eq('whatever@example.com')
+    expect(User.count - @starting_user_count).to eq(1)
+    expect(User.last.email).to eq("whatever@example.com")
+  end
+end
