b66b4f7f8f033ae6dc1e46c8edea01b5a5025c4e
Andrew Reid <areid.sj@gmail.com>
2020-08-20 12:43:14 -0600

    Add error method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :bids, dependent: :destroy
 
   has_secure_password
+
+  def error
+    return 'This email is already registered.' if User.find_by(email: email)
+
+    return 'Passwords must match.' if password != password_confirmation
+
+    return 'Complete all fields.' if !email || !password || !first_name || !last_name
+
+    'Something went wrong. Please double check your email and password and try again.'
+  end
+
 end
