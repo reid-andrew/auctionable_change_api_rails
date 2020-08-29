1dbbed64db463214775c322d51cb568e832f480d
Andrew Reid <areid.sj@gmail.com>
2020-08-29 10:25:03 -0600

    Write test for logging out user

---
+++ spec/api/v1/users/sessions/logout_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Login Endpoint - ', type: :request do
+  before(:each) do
+    @user = User.create(
+      first_name: 'Joe',
+      last_name: 'Strummer',
+      email: 'whatever@example.com',
+      password: 'pw123',
+      password_confirmation: 'pw123'
+    )
+    @user.save
+
+    post '/api/v1/sessions', params:
+      {
+        'email': 'whatever@example.com',
+        'password': 'pw123'
+      }
+  end
+
+  it 'logs out a user' do
+    expect(session[:user_id]).to eq(@user.id)
+
+    delete '/api/v1/sessions', params:
+      {
+        'email': 'whatever@example.com'
+      }
+
+    output = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(output[:data][:attributes][:message]).to eq('Successfully logged out')
+    expect(session[:user_id]).to eq(nil)
+  end
+
+
+
+
+
+
+end
